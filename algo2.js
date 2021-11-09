461c00d5c7b0afb55e550436de8606265b3dc552
VINAY <69250103+VINAY-YADDANA@users.noreply.github.com>
2021-11-09 13:20:59 +0530

    Create algo2.js

---
+++ algo2.js
@@ -0,0 +1,32 @@
+import _ from 'underscore'
+
+function getHops(graph, source, target, hops = 0, visited = []) {
+  if (source === target) {
+    return hops
+  }
+  if (_.includes(visited, source)) {
+    return null
+  }
+  let newVisited = visited.slice(0) // clone array
+  newVisited.push(source)
+  const hopsList = graph.successors(source).map((successor) => {
+    return getHops(graph, successor, target, hops + 1, newVisited)
+  })
+  return hopsList.length
+    ? _.min(hopsList.filter(hops => hops !== null))
+    : null
+}
+
+function getTrust(graph, source, target) {
+  const hops  = getHops(graph, source, target)
+  return hops === null
+    ? 0
+    : 1 / (1 + hops)
+}
+
+export default getTrust
+
+
+
+// WEBPACK FOOTER //
+// ./src/lib/algorithms/alg-2.js
